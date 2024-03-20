8d58c54dabf969810b1a77b3539a88a09f794476
Flame-eth <theflamepen@gmail.com>
2024-03-20 10:48:18 +0100

    Update file names and add new files

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,24 +1,24 @@
 import Logo from "./Icon.svg";
 import HeroGlow from "./heroGlow.svg";
 import Hawkins from "./Hawkins.png";
-import Alexander from "./alexander.svg";
-import Airbnb from "./airbnb.svg";
-import Google from "./google.svg";
-import Flutterwave from "./flutterwave.svg";
-import Binance from "./binance.svg";
-import Tesla from "./tesla.svg";
+import Alexander from "./Alexander.svg";
+import Airbnb from "./Airbnb.svg";
+import Google from "./Google.svg";
+import Flutterwave from "./Flutterwave.svg";
+import Binance from "./Binance.svg";
+import Tesla from "./Tesla.svg";
 import HelpingHands from "./hands-helping.svg";
 import BrainCircuit from "./brain-circuit.svg";
 import Brainstorm from "./brainstorm.svg";
 import Customize from "./customize.svg";
 import Diversity from "./diversity.svg";
 import Skills from "./skills.svg";
-import UnderlineCurve from "./underlinecurve.svg";
+import UnderlineCurve from "./UnderlineCurve.svg";
 import Nguyen from "./nguyen.svg";
 import Project from "./project.svg";
-import Sparkle from "./sparkle.svg";
-import Star from "./star.svg";
-import Solar from "./solar.svg";
+import Sparkle from "./Sparkle.svg";
+import Star from "./Star.svg";
+import Solar from "./Solar.svg";
 import Kayla from "./kayla.svg";
 import Coinbase from "./coinbase.svg";
 import Jerome from "./Jerome.svg";
@@ -28,50 +28,49 @@ import ProjectGear from "./projectGear.svg";
 import JobsHero1 from "./jobsHero1.svg";
 import JobsHero2 from "./jobsHero2.svg";
 import Dart from "./dart.svg";
-import AboutConf from './about.svg'
-import JeromeSingature from './JeromeSignature.svg'
-import Teamwork from './teamwork.jpg'
-import OverviewNote from './overviewNote.svg'
-import OverviewSearch from './overviewSearch.svg'
-import Support from './support.svg'
-import WavingHand from "./wavingHand.svg"
-import Chris from "./Chris.svg"
-import Devon from "./Devon.svg"
-import LoginMap from "./loginMap.png"
-import user1 from "./user1.svg"
-import user2 from "./user2.svg"
-import user3 from "./user3.svg"
-import user4 from "./user4.svg"
-import Dmitry from "./Dmitry.svg"
-import uploadMedia from "./uploadMedia.svg"
-import Theresa from "./Theresa.svg"
-import XhibitAd from "./xhibitAd.svg"
-import MaskAd from "./maskAd.svg"
-import Like from "./Like.svg"
-import Comment from "./Comment.svg"
-import Share from "./Share.svg"
-import UserBanner from "./UserBanner.svg"
-import FigmaProject from "./FigmaProject.svg"
-import tools from './tools.svg' 
-import certification from "./Certification.svg"
-import projects from "./projects.svg"
-import search from './search.svg'
-import role from "./role.svg"
-import headline from "./headline.svg"
-import empty from "./empty.svg"
-import instagram from "./instagram.svg"
-import linkedin from "./linkedin.svg"
-import twitter from "./twitter.svg"
-import facebook from "./facebook.svg"
-import interview from "./interview.svg"
-import startups from "./startups.svg"
-import coinbaseProject from "./coinbaseProject.svg"
-import aisight from "./aisight.svg"
-import soundhound from "./soundhound.svg"
-import peopleai from "./peopleai.svg"
-import blog from "./blog.svg"
-import blogImage from "./blogImg.svg"
-
+import AboutConf from "./about.svg";
+import JeromeSingature from "./JeromeSignature.svg";
+import Teamwork from "./teamwork.jpg";
+import OverviewNote from "./overviewNote.svg";
+import OverviewSearch from "./overviewSearch.svg";
+import Support from "./support.svg";
+import WavingHand from "./wavingHand.svg";
+import Chris from "./Chris.svg";
+import Devon from "./Devon.svg";
+import LoginMap from "./loginMap.png";
+import user1 from "./user1.svg";
+import user2 from "./user2.svg";
+import user3 from "./user3.svg";
+import user4 from "./user4.svg";
+import Dmitry from "./Dmitry.svg";
+import uploadMedia from "./uploadMedia.svg";
+import Theresa from "./Theresa.svg";
+import XhibitAd from "./XhibitAd.svg";
+import MaskAd from "./MaskAd.svg";
+import Like from "./Like.svg";
+import Comment from "./Comment.svg";
+import Share from "./Share.svg";
+import UserBanner from "./UserBanner.svg";
+import FigmaProject from "./FigmaProject.svg";
+import tools from "./tools.svg";
+import certification from "./Certification.svg";
+import projects from "./projects.svg";
+import search from "./search.svg";
+import role from "./role.svg";
+import headline from "./headline.svg";
+import empty from "./empty.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import facebook from "./facebook.svg";
+import interview from "./interview.svg";
+import startups from "./startups.svg";
+import coinbaseProject from "./coinbaseProject.svg";
+import aisight from "./aisight.svg";
+import soundhound from "./soundhound.svg";
+import peopleai from "./peopleai.svg";
+import blog from "./blog.svg";
+import blogImage from "./blogImg.svg";
 
 export {
   Logo,
@@ -104,7 +103,7 @@ export {
   JobsHero1,
   JobsHero2,
   Dart,
-  AboutConf, 
+  AboutConf,
   JeromeSingature,
   Teamwork,
   OverviewNote,
@@ -115,8 +114,8 @@ export {
   Devon,
   LoginMap,
   user1,
-  user2, 
-  user3, 
+  user2,
+  user3,
   user4,
   Dmitry,
   uploadMedia,
@@ -146,5 +145,5 @@ export {
   soundhound,
   peopleai,
   blog,
-  blogImage
+  blogImage,
 };
