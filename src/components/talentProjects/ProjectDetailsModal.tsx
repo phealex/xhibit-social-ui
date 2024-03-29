ab2c0f4f4bfb60eb84a943934c5a01e1cbcdea86
Flame-eth <theflamepen@gmail.com>
2024-03-30 00:16:58 +0100

    Refactor code to improve performance and readability

---
+++ src/components/talentProjects/ProjectDetailsModal.tsx
@@ -0,0 +1,63 @@
+import { FC } from "react";
+import { Dialog, DialogContent } from "../ui/dialog";
+import { useProjectDisplay } from "@/store";
+import { projects } from "@/constants";
+import { Mail, MapPin, Newspaper } from "lucide-react";
+import { Button } from "../ui/button";
+
+const ProjectDetailsModal: FC = () => {
+  const showProjectDetails = useProjectDisplay(
+    (state) => state.showProjectDetails
+  );
+  const projectToShowId = useProjectDisplay((state) => state.projectToShowId);
+  const project = projects.find((project) => project.id === projectToShowId);
+  return (
+    <div className="">
+      {project?.id ? (
+        <Dialog
+          open={showProjectDetails}
+          onOpenChange={() => {
+            useProjectDisplay.setState({ showProjectDetails: false });
+          }}
+        >
+          <DialogContent className="p-10 flex flex-col gap-[80px] justify-center items-center">
+            <div className="flex gap-10 flex-col items-center">
+              <div className="flex flex-col gap-5 items-center">
+                <div className="flex items-center flex-col gap-5">
+                  <img src={project.userImage} alt="" className=" h-20 w-20" />
+                  <div className="flex flex-col items-center gap-2">
+                    <h1 className=" font-Jakarta font-semibold text-2xl text-dark_green">
+                      {project?.username}
+                    </h1>
+                    <p className="flex gap-2 items-center font-Jakarta font-medium text-base text-home_grey">
+                        <MapPin className="h-5 w-5 text-home_grey "/>
+                        Lagos, NG
+                    </p>
+                  </div>
+                </div>
+                <p className=" bg-primary_blue/5 px-[10px] py-[5px] font-Jakarta font-medium text-xs text-dark_green/70  ">
+                  3 Projects completed on XHIBIT
+                </p>
+              </div>
+              <div className="flex items-center gap-[50px] ">
+                <Button className=" bg-dark_green/5 py-1 h-7 px-5 flex items-center gap-1 rounded-md hover:bg-dark_green/5 font-Jakarta font-medium text-xs text-dark_green/70 " >
+                    <Mail className="h-4 w-4 text-dark_green/70" />
+                    Hire talent
+                </Button>
+
+                <Button className=" bg-dark_green/5 py-1 h-7 px-5 flex items-center gap-1 rounded-md hover:bg-dark_green/5 font-Jakarta font-medium text-xs text-dark_green/70 " >
+                    <Newspaper className="h-4 w-4 text-dark_green/70" />
+                    View work profile
+                </Button>
+
+              </div>
+            </div>
+            <div className=""></div>
+          </DialogContent>
+        </Dialog>
+      ) : null}
+    </div>
+  );
+};
+
+export default ProjectDetailsModal;
