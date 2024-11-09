f2182ac8c37859fc496930b7ebde2ac84bd86bc9
Flame-eth <theflamepen@gmail.com>
2024-11-09 21:06:57 +0100

    chore: Enhance TalentDetails component with user state management and terms agreement

--- src/components/auth/TalentDetails.tsx
+++ src/components/auth/TalentDetails.tsx
@@ -1,6 +1,6 @@
 import { handleNextProps, userDetailsRegisterSchema } from "@/types";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import JoinBanner from "./JoinBanner";
@@ -16,8 +16,8 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Checkbox } from "../ui/checkbox";
 import { Link } from "react-router-dom";
-import { useAuthState } from "@/store";
-import { EnumUserUserType, Register } from "@/__generated__/graphql";
+import { useAuthState, useUserState } from "@/store";
+import { EnumUserUserType } from "@/__generated__/graphql";
 import { useMutation } from "@apollo/client";
 import { REGISTER } from "@/apollo/operations";
 import { useToast } from "../ui/use-toast";
@@ -31,10 +31,17 @@ const TalentDetails: FC<handleNextProps> = ({ handleNext }) => {
   const { toast } = useToast();
   const authData = useAuthState((state) => state.authData);
   const setAuthData = useAuthState((state) => state.setAuthData);
+  const setUserType = useUserState((state) => state.setUserType);
+  const setUser = useUserState((state) => state.setUser);
+
+  const [terms, setTerms] = useState<boolean>(false);
 
   const [register, { loading }] = useMutation(REGISTER, {
     onCompleted: (data) => {
       console.log(data);
+      localStorage.setItem("token", data.register.accessToken ?? "");
+      setUser(data.register);
+      setUserType(data.register.type as EnumUserUserType);
       handleNext();
     },
     onError: (error) => {
@@ -49,18 +56,46 @@ const TalentDetails: FC<handleNextProps> = ({ handleNext }) => {
 
   function onSubmit(data: z.infer<typeof userDetailsRegisterSchema>) {
     // console.log(data);
+
+    if (!terms) {
+      toast({
+        title: "Terms and conditions",
+        description: "Please agree to the terms and conditions",
+        variant: "destructive",
+      });
+      return;
+    }
     setAuthData({
       ...authData,
       ...data,
     });
 
     if (!authData) return;
+
+    // register({
+    //   variables: {
+    //     credentials: {
+    //       email: data.email,
+    //       firstName: data.firstName,
+    //       lastName: data.lastName,
+    //       password: data.password,
+    //       phone: data.phone,
+    //       userType: authData.userType as EnumUserUserType,
+    //       discipline: authData.discipline,
+    //       employmentType: authData.employmentType,
+    //       experience: authData.experience,
+
+    //     }
+    //   },
+    // });
+
     register({
       variables: {
         credentials: {
           ...authData,
           ...data,
-        } as Register,
+          userType: authData.userType as EnumUserUserType,
+        },
       },
     });
   }
@@ -170,7 +205,7 @@ const TalentDetails: FC<handleNextProps> = ({ handleNext }) => {
 
           <FormField
             control={form.control}
-            name="phoneNumber"
+            name="phone"
             render={({ field }) => (
               <FormItem>
                 <FormLabel className=" font-Jakarta font-medium text--[16px] text-dark_green ">
@@ -193,18 +228,12 @@ const TalentDetails: FC<handleNextProps> = ({ handleNext }) => {
             )}
           />
 
-          <FormField
+          {/* <FormField
             control={form.control}
             name="terms"
             render={({ field }) => (
               <FormItem>
-                <FormControl>
-                  <Checkbox
-                    className=" bg-white border border-[#8E9FAA] w-4 h-4 mr-2 data-[state=checked]:bg-white data-[state=checked]:text-primary_blue"
-                    checked={field.value}
-                    onCheckedChange={field.onChange}
-                  />
-                </FormControl>
+                <FormControl></FormControl>
                 <FormLabel
                   className=" font-Jakarta text-dark_green/70 font-normal text-[13px]"
                   htmlFor="terms"
@@ -215,21 +244,34 @@ const TalentDetails: FC<handleNextProps> = ({ handleNext }) => {
                 <FormMessage className="text-[#E75F51] font-Jakarta text-[13px] font-light" />
               </FormItem>
             )}
-          />
+          /> */}
+          <div className="flex items-center gap-2 ">
+            <Checkbox
+              className=" bg-white border border-[#8E9FAA] w-4 h-4 mr-2 data-[state=checked]:bg-white data-[state=checked]:text-primary_blue"
+              checked={terms}
+              id="terms"
+              onCheckedChange={(checked) =>
+                setTerms(checked.valueOf() as boolean)
+              }
+            />
+            <FormLabel
+              className=" font-Jakarta text-dark_green/70 font-normal text-[13px]"
+              htmlFor="terms"
+            >
+              Agree to <span className=" text-primary_blue">Terms</span> and{" "}
+              <span className=" text-primary_blue">Policy</span>
+            </FormLabel>
+          </div>
 
           <Button
             type="submit"
             className=" w-full  py-4 bg-primary_blue text-white hover:bg-primary_blue hover:text-white font-Jakarta text-[16px] font-medium rounded-md"
           >
-            {
-              loading ? (
-                <ClipLoader
-                  color="#ffffff"
-                  loading={loading}
-                  size={20}
-                />
-              ) : "Next"
-            }
+            {loading ? (
+              <ClipLoader color="#ffffff" loading={loading} size={20} />
+            ) : (
+              "Next"
+            )}
           </Button>
 
           <p className="flex w-full gap-1 items-center justify-center text-center font-Jakarta text-[16px] text-dark_green">
